186b386e1027a406ab2a817daf0ef441e61dc52d
Lothar Soto Palma <lsp.lot94@gmail.com>
2016-11-23 14:20:30 +0100

    Archivos jar y demases

--- Practica 3/MathIndexer/src/mathindexer/MathIndexer.java
+++ Practica 3/MathIndexer/src/mathindexer/MathIndexer.java
@@ -5,6 +5,20 @@
  */
 package mathindexer;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.tika.exception.TikaException;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.parser.Parser;
+import org.apache.tika.sax.BodyContentHandler;
+import org.xml.sax.SAXException;
+
 /**
  *
  * @author lot94
@@ -14,8 +28,33 @@ public class MathIndexer {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+        Parser parser = new AutoDetectParser();
+        Metadata metadata = new Metadata();
+        BodyContentHandler handler = new BodyContentHandler(-1);
+        InputStream stream = new FileInputStream("../Data/A-new-direct-time-integration-method-for-the-semi-discrete-parabolic-equations_2016_Engineering-Analysis-with-Boundary-Elements.pdf");
+        ParseContext context = new ParseContext();
+        
+        try {
+            parser.parse(stream, handler, metadata, context);
+
+        }catch(Exception e){
+            System.out.println("Error en el parser");
+        }
+        
+        stream.close();
+        
+        File f = new File("output3.txt");
+        FileWriter bw = new FileWriter(f);
+        bw.write(handler.toString());
+        
+        bw.close();
+
+        File f2 = new File("metadata.txt");
+        FileWriter bw2 = new FileWriter(f2);
+        bw2.write(metadata.toString());
+        
+        bw2.close();
     }
     
 }

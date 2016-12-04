28bd0284abd2a1070f0846f666e1ec9343b26bc4
JCEntrena <jentrena@correo.ugr.es>
2016-12-04 15:09:53 +0200

    Switch-Case para indexar los distintos campos.
    Se distingue cada campo según su posición de forma manual dentro del
    switch-case.

--- Practica 3/MathIndexer/src/mathindexer/MathIndexer.java
+++ Practica 3/MathIndexer/src/mathindexer/MathIndexer.java
@@ -19,6 +19,9 @@ import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
+import org.apache.lucene.document.IntPoint;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.FSDirectory;
@@ -41,7 +44,6 @@ public class MathIndexer {
     public MathIndexer(String collectionPath) throws IOException{
         Path path = FileSystems.getDefault().getPath("testIndex");
 
-
         this.version = Version.LUCENE_6_2_1;
         this.collectionPath = collectionPath;
         this.dir = FSDirectory.open(path);
@@ -55,15 +57,87 @@ public class MathIndexer {
         
     }
     
-    public void readAndIndex(String fileName) throws FileNotFoundException{
+    public void readAndIndex(String fileName) throws FileNotFoundException, IOException{
         File file = new File(fileName);
         Scanner inputStream = new Scanner(file);
+        // Saltamos la primera línea. 
+        inputStream.nextLine(); 
+        
+        Document doc = new Document(); 
+        
         while(inputStream.hasNext()){
             String data = inputStream.nextLine();
             String[] parts = data.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)"); 
-            for(int i = 0; i < parts.length; i++)
-                System.out.println(parts[i]);
+            for(int i = 0; i < parts.length; i++){
+                // Eliminamos comillas para aquellos que no se tokenicen. 
+                parts[i] = parts[i].replace("\"", ""); 
+                
+                // Switch - case que distingue los campos almacenados en el índice. 
+                switch (i) {
+                    case 0: 
+                        doc.add(new StringField("Autor", parts[i], Field.Store.YES)); 
+                        break; 
+                    case 1:  
+                        doc.add(new TextField("Título", parts[i], Field.Store.YES)); 
+                        break;
+                    case 2:  
+                        // ParseInt no funciona con el string vacío, así que distinguimos casos. 
+                        int year; 
+                            if (!"".equals(parts[i]))
+                                year = Integer.parseInt(parts[i]); 
+                            else
+                                year = 0; 
+                        doc.add(new IntPoint("Año", year)); 
+                        
+                        break;
+                    case 3: 
+                        doc.add(new StringField("Fuente", parts[i], Field.Store.YES)); 
+                        break; 
+                    case 4: 
+                        int pageS; 
+                            if (!"".equals(parts[i]))
+                                pageS = Integer.parseInt(parts[i]); 
+                            else
+                                pageS = 0; 
+                        doc.add(new IntPoint("Página inicio", pageS)); 
+                        break; 
+                    case 5: 
+                        int pageE; 
+                            if (!"".equals(parts[i]))
+                                pageE = Integer.parseInt(parts[i]); 
+                            else
+                                pageE = 0; 
+                        doc.add(new IntPoint("Página fin", pageE)); 
+                        break; 
+                    case 6: 
+                        doc.add(new StringField("Link", parts[i], Field.Store.YES)); 
+                        break; 
+                    case 7: 
+                        doc.add(new TextField("Abstract", parts[i], Field.Store.NO)); 
+                        break; 
+                    case 8: 
+                        doc.add(new TextField("Palabras clave autor", parts[i], Field.Store.YES));
+                        break;
+                    case 9: 
+                        doc.add(new TextField("Palabras clave índice", parts[i], Field.Store.YES));
+                        break;
+                    case 10: 
+                        doc.add(new StringField("Referencias", parts[i], Field.Store.YES)); 
+                        break; 
+                    case 11: 
+                        doc.add(new StringField("Idioma", parts[i], Field.Store.YES)); 
+                        break; 
+                    case 12: 
+                        doc.add(new StringField("Tipo de documento", parts[i], Field.Store.YES));
+                        break; 
+                    default: 
+                        break;                                                                
+                } 
+               
+            }
         }
+        writer.addDocument(doc); 
+        writer.close(); 
         inputStream.close();   
     }
     
@@ -72,7 +146,7 @@ public class MathIndexer {
      */
     public static void main(String[] args) throws IOException {
         MathIndexer test = new MathIndexer("../Data");
-        test.readAndIndex("../Data/Modified.csv");
+        test.readAndIndex("../Data/Cortado.csv");
     }
     
 }

eadc16d63a13868be4d4694a881ab781dd52e644
JCEntrena <jentrena@correo.ugr.es>
2016-12-07 18:49:27 +0200

    Cambio en nombre de archivo final

--- Practica 3/Data/deleteRows.rb
+++ Practica 3/Data/deleteRows.rb
@@ -11,7 +11,7 @@ def selector
     modified_data << [row[0], row[1], row[2], row[3], row[7], row[8], row[12], row[13], row[14], row[15], row[16], row[17], row[18]]
   end
 
-  CSV.open("Modified.csv", "wb") do |csv|
+  CSV.open("Final.csv", "wb") do |csv|
     modified_data.each do |row|
       csv << row
     end

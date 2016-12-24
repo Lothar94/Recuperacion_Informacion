8b950b43f3e1ba36cbdc2c919fa4b5dcd5121d31
Lothar Soto Palma <lsp.lot94@gmail.com>
2016-12-24 13:24:19 +0100

    Eliminado main de mathsearcher

--- Practica 3/MathSearcher/src/mathsearcher/MathSearcher.java
+++ Practica 3/MathSearcher/src/mathsearcher/MathSearcher.java
@@ -278,10 +278,4 @@ public class MathSearcher{
     public ArrayList<FacetResult> getFacets(){
         return facetResults;
     }
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws IOException, FileNotFoundException, ParseException {
-    }
-
 }
